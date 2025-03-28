070a6fb46b38169adb2f505be1398e324d71ad62
sudip-codencoit <sudip.mandal@codencoit.com>
2025-03-28 06:20:39 +0600

    sd

--- routes/payment.js
+++ routes/payment.js
@@ -53,7 +53,7 @@ router.post("/new_payment", userAuth, async (req, res) => {
 
     // const amount = package.find((p) => p.id === packageid).priceINR;
     // const usdAmount = package.find((p) => p.id === packageid).priceUSD;
-    const amount = 1;
+    const amount = 5;
     const usdAmount = 0.05;
     if (!amount || !usdAmount) {
       return res.status(400).json({ message: "Invalid package" });
