5ee24add99ef9e8541ce9b736ecdc260112c9390
sudip-codencoit <sudip.mandal@codencoit.com>
2025-04-04 04:42:12 +0600

    sd

--- routes/payment.js
+++ routes/payment.js
@@ -410,6 +410,7 @@ router.post("/cryptomous_hook", async (req, res) => {
   try {
     const signature = req.headers.sign;
 console.log('sudip')
+console.log(headers)
     if (!signature) {
       return res.status(400).json({
         success: false,
